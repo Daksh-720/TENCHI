6babb9c9fc478d59089081c12100175bd2776b70
Daksh <dakshsalvi59@gmail.com>
2026-07-31 15:34:26 +0530

    Adding a new class

---
+++ src/main/java/com/daksh/springboard/service/ClipService.java
@@ -0,0 +1,10 @@
+package com.daksh.springboard.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClipService {
+    public String message(){
+        return "Hello from ClipService";
+    }
+}
