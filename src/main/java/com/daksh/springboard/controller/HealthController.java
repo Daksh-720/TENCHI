835dc131774a84bd9558bc0e63b127434260c612
Daksh <dakshsalvi59@gmail.com>
2026-07-31 11:25:11 +0530

    Added HealthController

---
+++ src/main/java/com/daksh/springboard/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.daksh.springboard.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    
+    @GetMapping("/")
+    public String health(){
+        return "Tenchi's backend is running!!";
+    }
+}
