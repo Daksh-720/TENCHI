d047671dc0c0274dc3533c87dfde7a9b851edb15
Daksh <dakshsalvi59@gmail.com>
2026-07-31 15:34:26 +0530

    Adding a new class

--- src/main/java/com/daksh/springboard/controller/HealthController.java
+++ src/main/java/com/daksh/springboard/controller/HealthController.java
@@ -2,12 +2,20 @@ package com.daksh.springboard.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.daksh.springboard.service.ClipService;
 
 @RestController
 public class HealthController {
-    
+
+    private final ClipService clipService;
+
+    public HealthController(ClipService clipService){
+        this.clipService=clipService;
+    }
+
+
     @GetMapping("/")
     public String health(){
-        return "Tenchi's backend is running!!";
+        return clipService.message();
     }
 }
