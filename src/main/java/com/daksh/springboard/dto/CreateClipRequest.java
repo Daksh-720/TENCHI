6fcc9854c554b956612ef65c4a449e3a1ee83279
Daksh <dakshsalvi59@gmail.com>
2026-08-01 01:28:49 +0530

    Adding a new class

--- src/main/java/com/daksh/springboard/dto/CreateClipRequest.java
+++ src/main/java/com/daksh/springboard/dto/CreateClipRequest.java
@@ -2,4 +2,13 @@ package com.daksh.springboard.dto;
 
 public class CreateClipRequest {
     
+    private String content;
+
+
+    public String getContent(){
+        return content;
+    }
+    public void setContent(String content){
+        this.content=content;
+    }
 }
