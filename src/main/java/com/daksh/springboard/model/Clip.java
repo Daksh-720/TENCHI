e35237a92a2357b2f04f7e0a07823a00eedbd287
Daksh <dakshsalvi59@gmail.com>
2026-08-02 00:47:09 +0530

    Adding new class

---
+++ src/main/java/com/daksh/springboard/model/Clip.java
@@ -0,0 +1,13 @@
+package com.daksh.springboard.model;
+
+
+public class Clip {
+    private String content;
+
+    public String getContent(){
+        return content;
+    }
+    public void setContent(String content){
+        this.content = content;
+    }
+}
--- src/main/java/com/daksh/springboard/service/ClipService.java
+++ src/main/java/com/daksh/springboard/service/ClipService.java
@@ -1,12 +1,19 @@
 package com.daksh.springboard.service;
 
 import org.springframework.stereotype.Service;
-
 import com.daksh.springboard.dto.CreateClipRequest;
+import com.daksh.springboard.model.Clip;
+import java.util.*;
 
 @Service
 public class ClipService {
+
+    private final List<Clip> clips = new ArrayList<>();
+
     public void createClip(CreateClipRequest request){
-        System.out.println(request.getContent());
+        Clip clip = new Clip();
+        clip.setContent(request.getContent());
+        clips.add(clip);
+    
     }
 }
