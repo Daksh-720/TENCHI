cbc5801a36e73b003d99bda9c6fd86e26b40369a
Daksh <dakshsalvi59@gmail.com>
2026-08-03 22:56:04 +0530

    Changing content

--- src/main/java/com/daksh/springboard/controller/ClipController.java
+++ src/main/java/com/daksh/springboard/controller/ClipController.java
@@ -1,6 +1,7 @@
 package com.daksh.springboard.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,6 +47,12 @@ public class ClipController {
     @PutMapping("/clips/{id}")
     public ResponseEntity<GetClipResponse> updateClip(@PathVariable Long id, @RequestBody UpdateClipRequest request){
         return ResponseEntity.ok(clipService.updateClip(id, request));
+    }
+
+    @DeleteMapping("/clips/{id}")
+    public ResponseEntity<String> deleteClip(@PathVariable Long id){
+        clipService.deleteClip(id);
+        return ResponseEntity.ok("Clip deleted Successfully!!");
     }
 
 }
