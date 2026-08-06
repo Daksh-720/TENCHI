1706ca50a49781e951e28fb1bfae038e5a58849e
Daksh <dakshsalvi59@gmail.com>
2026-08-07 02:05:07 +0530

    Adding methods

--- src/main/java/com/daksh/springboard/util/CodeGenerator.java
+++ src/main/java/com/daksh/springboard/util/CodeGenerator.java
@@ -1,5 +1,20 @@
 package com.daksh.springboard.util;
 
+import java.security.SecureRandom;
+
 public class CodeGenerator {
-    
+
+    private static final String CHARACTERS = "abcxyz0123456789";
+
+    public String generate(){
+        StringBuilder code = new StringBuilder();
+        SecureRandom random = new SecureRandom();
+
+        for(int i=0; i<6; i++){
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            char randomCharacter = CHARACTERS.charAt(randomIndex);
+            code.append(randomCharacter);
+        }
+        return code.toString();
+    }
 }
