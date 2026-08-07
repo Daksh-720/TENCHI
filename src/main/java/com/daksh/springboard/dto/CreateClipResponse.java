b34e7418cf4c96e025146b7a5fcd7a30e1f65dfa
Daksh <dakshsalvi59@gmail.com>
2026-08-07 13:02:26 +0530

    refined sharecode methods

--- src/main/java/com/daksh/springboard/dto/CreateClipResponse.java
+++ src/main/java/com/daksh/springboard/dto/CreateClipResponse.java
@@ -3,6 +3,7 @@ package com.daksh.springboard.dto;
 public class CreateClipResponse {
 
     private Long id;
+    private String shareCode;
     private String message;
 
     public Long getId(){
@@ -16,6 +17,19 @@ public class CreateClipResponse {
         return message;
     }
     public void setMessage(String message){
-        this.message=message;
+        this.message = message;
+    }
+
+    public String getShareCode(){
+        return shareCode;
+    }
+    public void setShareCode(String shareCode){
+        this.shareCode = shareCode;
+    }
+
+    public CreateClipResponse(Long id, String shareCode, String message){
+        this.id = id;
+        this.shareCode = shareCode;
+        this.message = message;
     }
 }
