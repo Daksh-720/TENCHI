a10eb82a68206fb1deb3f0c0b358fc447868b710
Daksh <dakshsalvi59@gmail.com>
2026-08-08 17:41:14 +0530

    code for deleting data after expiry

--- src/main/java/com/daksh/springboard/SpringboardApplication.java
+++ src/main/java/com/daksh/springboard/SpringboardApplication.java
@@ -2,8 +2,11 @@ package com.daksh.springboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringboardApplication {
 
 	public static void main(String[] args) {
