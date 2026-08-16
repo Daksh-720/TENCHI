0f61f2ba8a9fd10bba5c8e9922d4d9bd3473670c
Daksh <dakshsalvi59@gmail.com>
2026-08-16 14:20:12 +0530

    Adding AuthService Class

---
+++ src/main/java/com/daksh/springboard/service/AuthService.java
@@ -0,0 +1,28 @@
+package com.daksh.springboard.service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import com.daksh.springboard.entity.User;
+import com.daksh.springboard.repository.UserRepository;
+
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService (UserRepository userRepository, PasswordEncoder passwordEncoder){
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    public User register(String username, String email, String password){
+        if(userRepository.findByEmail(email).isPresent()){
+            throw new RuntimeException("Email already registered!");
+        }
+        String hashedPassword = passwordEncoder.encode(password);
+        User user = new User(username, email, hashedPassword);
+        return userRepository.save(user);
+    }
+}
