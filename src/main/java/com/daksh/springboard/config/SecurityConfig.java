f16d3866741fd7fd2d97685a965cd45220df34d5
Daksh <dakshsalvi59@gmail.com>
2026-08-16 20:03:07 +0530

    Added a method in SecurityConfig

--- src/main/java/com/daksh/springboard/config/SecurityConfig.java
+++ src/main/java/com/daksh/springboard/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.daksh.springboard.config;
 // import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -12,4 +14,15 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/auth/**").permitAll()
+            .anyRequest().authenticated()
+        );
+    return http.build();
+   }
 }
