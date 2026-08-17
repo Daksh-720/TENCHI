0eaa13d3eae1e541b2a562f1b55a66c0606a0b26
Daksh <dakshsalvi59@gmail.com>
2026-08-18 00:01:52 +0530

    Slight modification in line spacing

--- src/main/java/com/daksh/springboard/exception/DuplicateEmailException.java
+++ src/main/java/com/daksh/springboard/exception/DuplicateEmailException.java
@@ -1,5 +1,7 @@
 package com.daksh.springboard.exception;
 
+
+
 public class DuplicateEmailException extends RuntimeException {
     public DuplicateEmailException(String message){
         super(message);
