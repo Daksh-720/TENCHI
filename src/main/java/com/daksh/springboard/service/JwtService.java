42c03787d467314b4596fe85bbbc14f5e2d72790
Daksh <dakshsalvi59@gmail.com>
2026-08-18 19:43:21 +0530

    Adding jwt algorithm

---
+++ src/main/java/com/daksh/springboard/service/JwtService.java
@@ -0,0 +1,18 @@
+package com.daksh.springboard.service;
+
+import javax.crypto.SecretKey;
+import org.springframework.stereotype.Service;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+
+@Service
+public class JwtService {
+    private final SecretKey secretKey = Keys.hmacShaKeyFor("your-secret-key-must-be-at-least-32-characters".getBytes());
+    public String generateToken(String email){
+        return Jwts.builder()
+                   .subject(email)
+                   .signWith(secretKey, Jwts.SIG.HS256)
+                   .compact();
+    }
+}
