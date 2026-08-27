3c41d1787ba03a59762b402547f8ab26279c0ad6
Daksh <dakshsalvi59@gmail.com>
2026-08-28 00:58:36 +0530

    Adding method to filter email from token

--- src/main/java/com/daksh/springboard/security/JwtAuthenticationFilter.java
+++ src/main/java/com/daksh/springboard/security/JwtAuthenticationFilter.java
@@ -1,9 +1,14 @@
 package com.daksh.springboard.security;
 
+import com.daksh.springboard.entity.User;
+import com.daksh.springboard.repository.UserRepository;
 import java.io.IOException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import com.daksh.springboard.service.JwtService;
+import java.util.Collections;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,10 +18,12 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final UserRepository userRepository;
     private final JwtService jwtService;
 
-    public JwtAuthenticationFilter(JwtService jwtService){
+    public JwtAuthenticationFilter(JwtService jwtService, UserRepository userRepository){
         this.jwtService = jwtService;
+        this.userRepository = userRepository;
     }
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -27,5 +34,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                     filterChain.doFilter(request, response);
                     return;
                 }
+                String token = authHeader.substring(7);
+                String email = jwtService.extractEmail(token);
+                User user = userRepository.findByEmail(email).orElse(null);
+                if(user != null){
+                    UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(user, null, Collections.emptyList());
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+                filterChain.doFilter(request, response);
     }
 }
