46b82c0d8d8e1ab58b430719c525a03e1019711b
Daksh <dakshsalvi59@gmail.com>
2026-08-29 21:27:35 +0530

    Created functions

---
+++ src/functions/Folder.jsx
@@ -0,0 +1,11 @@
+function Folder (){
+    return (
+        <div className="flex justify-center">
+            <button className="rounded-2xl border border-white/20 bg-white/10 px-8 py-4 text-white backdrop-blur-md transition hover:bg-white/20">
+             Select Folder
+            </button>
+        </div>
+    )
+}
+
+export default Folder
---
+++ src/functions/Video.jsx
@@ -0,0 +1,11 @@
+function Video(){
+    return (
+        <div className="flex justify-center">
+            <button className="rounded-2xl border border-white/20 bg-white/10 px-8 py-4 text-white backdrop-blur-md transition hover:bg-white/20">
+             Select Video
+            </button>
+        </div>
+    );
+}
+
+export default Video
