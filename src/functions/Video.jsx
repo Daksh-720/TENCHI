ac380257fd89b3c5afa28705f236022de33ef7fe
Daksh <dakshsalvi59@gmail.com>
2026-08-29 22:45:43 +0530

    Adding separate files to send just frontend

--- src/functions/Video.jsx
+++ src/functions/Video.jsx
@@ -1,10 +1,9 @@
 function Video(){
     return (
-        <div className="flex justify-center">
-            <button className="rounded-2xl border border-white/20 bg-white/10 px-8 py-4 text-white backdrop-blur-md transition hover:bg-white/20">
-             Select Video
-            </button>
-        </div>
+        <button className="flex h-27 w-27 flex-col items-center justify-center gap-3 rounded-2xl border border-white/20 bg-white/10 text-white backdrop-blur-md transition hover:bg-white/20">
+        <div className="text-3xl">🎥</div>
+        Video
+        </button>
     );
 }
 
