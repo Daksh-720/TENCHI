86af341f61c3ac4eb8fb4de4c87f74d54aa2d54f
Daksh <dakshsalvi59@gmail.com>
2026-08-30 23:25:04 +0530

    edited files

--- src/functions/Folder.jsx
+++ src/functions/Folder.jsx
@@ -1,10 +1,21 @@
-function Folder (){
+function Folder({ activeMode, setActiveMode, setFiles}){
+    function handleFolderChange(event){
+        setFiles(Array.from(event.target.files));
+        setActiveMode("folder");
+    }
+
+
     return (
-        <button className="flex h-27 w-27 flex-col items-center justify-center gap-3 rounded-2xl border border-white/20 bg-white/10 text-white backdrop-blur-md transition hover:bg-white/20">
+        <>
+        <input type="file" id="folderInput" webkitdirectory="" multiple onChange={handleFolderChange} className="hidden" />
+        <button
+        onClick={() => document.getElementById(folderInput).click()}
+        className="flex h-27 w-27 flex-col items-center justify-center gap-3 rounded-2xl border border-white/20 bg-white/10 text-white backdrop-blur-md transition hover:bg-white/20">
         <div className="text-3xl">📁</div>
         Folder
         </button>
-    )
+        </>
+    );
 }
 
 export default Folder
