57d4cd9735cfed50fb9bdd51584c7c709d75b6dc
Daksh <dakshsalvi59@gmail.com>
2026-08-31 14:17:32 +0530

    Adding readOnly method for generated codes

--- src/functions/SendRet.jsx
+++ src/functions/SendRet.jsx
@@ -1,8 +1,17 @@
 function SendRet(){
     return(
-        <button className="rounded-xl border-2 border-[#00D2FF]/60 bg-[#A78BFA]/10 px-8 py-3 text-white backdrop-blur-md transition hover:bg-[#737FF2]/40 hover:backdrop-blur-xl">
+        <div className="flex items-center gap-4">
+        <button className="rounded-xl border-2 border-[#00D2FF]/60 bg-[#A78BFA]/10 px-8 py-3 text-white backdrop-blur-md transition hover:bg-[#737FF2]/40 hover:backdrop-blur-xl cursor-pointer">
          SEND
         </button>
+
+        <input
+        type="text"
+        placeholder="Generated-code:"
+        readOnly
+        className="h-12 w-48 rounded-xl border border-white/20 bg-white/10 px-4 py-3 text-white backdrop-blur-md"
+        />
+        </div>
     );
 }
 
