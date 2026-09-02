b6fcee5e771f22c1547c79771680ba529012d135
Daksh <dakshsalvi59@gmail.com>
2026-09-03 01:49:37 +0530

    Added onClick event

--- src/functions/SendRet.jsx
+++ src/functions/SendRet.jsx
@@ -8,9 +8,16 @@ function SendRet(){
     const [expiryTime, setExpiryTime] = useState("");
     const [expiryUnit, setExpiryUnit] = useState("minutes");
 
+    function handleSend(){
+        const sendData = {
+            expiryTime,
+            expiryUnit
+        };
+    }
+
     return(
         <div className="flex items-center gap-4">
-        <button className="rounded-xl border-2 border-[#00D2FF]/60 bg-[#A78BFA]/10 px-8 py-3 text-white backdrop-blur-md transition hover:bg-[#737FF2]/40 hover:backdrop-blur-xl cursor-pointer">
+        <button onClick={handleSend} className="rounded-xl border-2 border-[#00D2FF]/60 bg-[#A78BFA]/10 px-8 py-3 text-white backdrop-blur-md transition hover:bg-[#737FF2]/40 hover:backdrop-blur-xl cursor-pointer">
          SEND
         </button>
 
