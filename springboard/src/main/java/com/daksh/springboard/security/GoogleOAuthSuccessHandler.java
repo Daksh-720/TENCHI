bd04ca5a2d211c9fc4f980005370b4fee9a0edd4
Daksh <dakshsalvi59@gmail.com>
2026-09-14 03:36:26 +0530

    Added auth package

--- springboard/src/main/java/com/daksh/springboard/security/GoogleOAuthSuccessHandler.java
+++ springboard/src/main/java/com/daksh/springboard/security/GoogleOAuthSuccessHandler.java
@@ -8,9 +8,22 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import com.daksh.springboard.entity.User;
+import com.daksh.springboard.repository.UserRepository;
+import com.daksh.springboard.service.AuthService;
 
 @Component
 public class GoogleOAuthSuccessHandler implements AuthenticationSuccessHandler{
+
+    private final AuthService authService;
+    private final UserRepository userRepository;
+
+    public GoogleOAuthSuccessHandler(UserRepository userRepository, AuthService authService){
+        this.userRepository=userRepository;
+        this.authService = authService;
+    }
+
+
     @Override
      public void onAuthenticationSuccess(
              HttpServletRequest request,
@@ -22,5 +35,13 @@ public class GoogleOAuthSuccessHandler implements AuthenticationSuccessHandler{
          String email = oauthUser.getAttribute("email");
          String name = oauthUser.getAttribute("name");
          
+         User user = userRepository.findByEmail(email)
+                                   .orElseGet(() -> userRepository.save(
+                                    new User(name, email, "")
+                                   ));
+
+        
+         String token = authService.generateToken(user.getEmail());
+         response.sendRedirect("http://localhost:5173?token=" + token);
     }
 }
