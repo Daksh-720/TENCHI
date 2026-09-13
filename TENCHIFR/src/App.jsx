07c453f2edefedabe1730bc5e23f855391d12cca
Daksh <dakshsalvi59@gmail.com>
2026-09-14 03:40:29 +0530

    Added Oauth method

--- TENCHIFR/src/App.jsx
+++ TENCHIFR/src/App.jsx
@@ -8,10 +8,10 @@ import Folder from "./functions/Folder";
 import Image from "./functions/Image";
 import Video from "./functions/Video";
 import Filess from "./functions/Filess";
-import SendRet from "./functions/SendRet";
 import ActivePanel from "./functions/ActivePanel";
 import Retrieval from "./components/Retrieval";
 import ScrollButton from "./components/ScrollButton";
+import OAuth from "./auth/OAuth";
 
 
 function App(){
@@ -21,6 +21,7 @@ function App(){
   return (
 
     <div className="relative min-h-screen overflow-hidden bg-black">
+      <OAuth />
     <div className="absolute top-6 right-6 z-50">
         <nav className="relative ml-auto flex w-fit items-center gap-2 rounded-3xl border border-white/10 bg-white/7 p-2 shadow-lg backdrop-blur-md">
         <HamBurgerMenu />
