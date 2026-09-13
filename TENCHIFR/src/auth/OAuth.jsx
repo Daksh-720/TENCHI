8cf2f803e6fdb175d90e4702173f0442933a6e09
Daksh <dakshsalvi59@gmail.com>
2026-09-14 03:36:26 +0530

    Added auth package

---
+++ TENCHIFR/src/auth/OAuth.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+
+
+
+function OAuth() {
+
+    useEffect(() => {
+        const params = new URLSearchParams(window.location.search);
+        const token = params.get("token");
+
+        if(token) {
+            localStorage.setItem("token", token);
+            window.history.replaceState({}, document.title, window.location.pathname);
+        }
+    }, []);
+
+    function handleGoogleLogin() {
+        window.location.href = "http://localhost:8080/oauth2/authorization/google";
+    }
+    return null;
+}
+
+export default OAuth
