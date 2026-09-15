e719ebd04c2daded844eeeab8b812423c3db2de3
Daksh <dakshsalvi59@gmail.com>
2026-09-15 23:15:27 +0530

    Added method for git auth

--- springboard/src/main/java/com/daksh/springboard/security/GitHubOAuthSuccessHandler.java
+++ springboard/src/main/java/com/daksh/springboard/security/GitHubOAuthSuccessHandler.java
@@ -1,8 +1,24 @@
 package com.daksh.springboard.security;
 
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 @Component
-public class GitHubOAuthSuccessHandler {
+public class GitHubOAuthSuccessHandler implements AuthenticationSuccessHandler {
     
+    @Override 
+    public void onAuthenticationSuccess(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        Authentication authentication) throws IOException, ServletException {
+            OAuth2User oauthUser = (OAuth2User) authentication.getPrincipal();
+        }
 }
