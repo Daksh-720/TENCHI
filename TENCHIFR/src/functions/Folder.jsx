18e58db371016d8b46f30d476e93bfbd34f3818b
Daksh <dakshsalvi59@gmail.com>
2026-09-17 02:28:41 +0530

    Added to app.jsx

--- TENCHIFR/src/functions/Folder.jsx
+++ TENCHIFR/src/functions/Folder.jsx
@@ -1,4 +1,4 @@
-function Folder({ activeMode, setActiveMode, setFiles}){
+function Folder({ activeMode, setActiveMode, setFiles, darkMode }) {
     function handleFolderChange(event){
         setFiles(Array.from(event.target.files));
         setActiveMode("folder");
@@ -11,7 +11,11 @@ function Folder({ activeMode, setActiveMode, setFiles}){
         <button
         type="button"
         onClick={() => document.getElementById("folderInput").click()}
-        className="flex h-27 w-27 flex-col items-center justify-center gap-3 rounded-2xl border border-white/20 bg-white/10 text-white backdrop-blur-md transition hover:bg-white/20">
+        className={
+                    darkMode
+                        ? "flex h-27 w-27 flex-col items-center justify-center gap-3 rounded-2xl border border-white/20 bg-white/10 text-white backdrop-blur-md transition hover:bg-white/20 cursor-pointer"
+                        : "flex h-27 w-27 flex-col items-center justify-center gap-3 rounded-2xl border border-black/25 bg-white/30 text-black backdrop-blur-md shadow-lg transition hover:bg-white/40 cursor-pointer"
+                }>
         <div className="text-3xl">📁</div>
         Folder
         </button>
