db5e01061ccab518a154583f0ac92302a6687b28
Daksh <dakshsalvi59@gmail.com>
2026-09-17 02:32:15 +0530

    Added css to all files for lighttheme

--- TENCHIFR/src/functions/Image.jsx
+++ TENCHIFR/src/functions/Image.jsx
@@ -1,4 +1,4 @@
-function Image({ activeMode, setActiveMode, setFiles }){
+function Image({ activeMode, setActiveMode, setFiles, darkMode }){
     function handleImageChange(event){
         setFiles(Array.from(event.target.files));
         setActiveMode("image");
@@ -11,7 +11,11 @@ function Image({ activeMode, setActiveMode, setFiles }){
         <button
         type="button"
         onClick={() => document.getElementById("imageInput").click()}
-        className="flex h-27 w-27 flex-col items-center justify-center gap-3 rounded-2xl border border-white/20 bg-white/10 text-white backdrop-blur-md transition hover:bg-white/20">
+        className={
+                    darkMode
+                        ? "flex h-27 w-27 flex-col items-center justify-center gap-3 rounded-2xl border border-white/20 bg-white/10 text-white backdrop-blur-md transition hover:bg-white/20 cursor-pointer"
+                        : "flex h-27 w-27 flex-col items-center justify-center gap-3 rounded-2xl border border-black/25 bg-white/30 text-black backdrop-blur-md shadow-lg transition hover:bg-white/40 cursor-pointer"
+                }>
         <div className="text-3xl">🖼️</div>
         Image
         </button>
