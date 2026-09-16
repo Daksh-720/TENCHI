d0d4452fa2c568ef07f0fee2f2b1509e80d8f544
Daksh <dakshsalvi59@gmail.com>
2026-09-17 02:32:15 +0530

    Added css to all files for lighttheme

--- TENCHIFR/src/functions/Video.jsx
+++ TENCHIFR/src/functions/Video.jsx
@@ -1,4 +1,4 @@
-function Video({ activeMode, setActiveMode, setFiles }){
+function Video({ activeMode, setActiveMode, setFiles, darkMode }){
     function handleVideoChange(event){
         setFiles(Array.from(event.target.files));
         setActiveMode("video");
@@ -12,7 +12,11 @@ function Video({ activeMode, setActiveMode, setFiles }){
         <button
         type="button"
         onClick={() => document.getElementById("videoInput").click()}
-        className="flex h-27 w-27 flex-col items-center justify-center gap-3 rounded-2xl border border-white/20 bg-white/10 text-white backdrop-blur-md transition hover:bg-white/20">
+        className={
+                    darkMode
+                        ? "flex h-27 w-27 flex-col items-center justify-center gap-3 rounded-2xl border border-white/20 bg-white/10 text-white backdrop-blur-md transition hover:bg-white/20 cursor-pointer"
+                        : "flex h-27 w-27 flex-col items-center justify-center gap-3 rounded-2xl border border-black/25 bg-white/30 text-black backdrop-blur-md shadow-lg transition hover:bg-white/40 cursor-pointer"
+                }>
         <div className="text-3xl">🎥</div>
         Video
         </button>
